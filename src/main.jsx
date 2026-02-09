370285e3ec0bb55a36d830af894239283a19ec3c
jhantu626 <jhantubala626@gmail.com>
2026-02-09 16:29:50 +0530

    user page route

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import {
   Login,
   NotFound,
   Templetes,
+  Users,
 } from "./pages";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import ProtectedRoute from "./pages/ProtectedRoute.jsx";
@@ -35,6 +36,7 @@ const router = createBrowserRouter(
           <Route path="/" element={<Dashboard />} />
           <Route path="/templates" element={<Templetes />} />
           <Route path="/create-templates" element={<CreateTemplates />} />
+          <Route path="/users" element={<Users />} />
         </Route>
       </Route>
       <Route path="*" element={<NotFound />} />
